f666324d8b1b63305ecbab29758c559345ad857e
rleongPd5 <rleong00@stuy.edu>
2019-11-09 17:23:44 -0500

    Starting to test if rand reads properly

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "randfile.h"
+
+void printbar(){
+  printf("============================================================================\n", );
+}
+int main(){
+  printbar();
+  printf("Testing rand\n");
+  printbar();
+  printf("Random number generated: %d\n", rand());
+}
