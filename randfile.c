c00efc22d7eeeb6459f0ddf630e684965e0771dc
rleongPd5 <rleong00@stuy.edu>
2019-11-09 17:46:45 -0500

    Fixed compiling errors, but it isn't opening correctly

--- randfile.c
+++ randfile.c
@@ -6,23 +6,22 @@
 //A function that generates a random number by reading in the correct number of bytes from /dev/random.
 int rand()
 {
-  int randomData = open("/dev/urandom", O_RDONLY); //open file
-  if (randomData < 0)
+int randomData = open("/dev/urandom", O_RDONLY); //open file
+if (randomData < 0)
+  {
+      // ERROR
+      printf("Open errno %d error: %s\n", errno, strerror(errno));
+      return 0;
+  }
+  else
+  {
+    int *rnd;
+    int test = read(randomData, rnd, sizeof(rnd));
+    if (test < 0)
     {
-        // ERROR
-        printf("errno %d error: %s\n, errno, strerror(errno));
+        // returning -1 and sets errno if unsuccessful
+        printf("Read errno %d error: %s\n", errno, strerror(errno));
         return 0;
-    }
-    else
-    {
-      int rnd;
-      int test = read(randomData, rnd, sizeof(rnd));
-      if (test < 0)
-      {
-          // returning -1 and sets errno if unsuccessful
-          printf("errno %d error: %s\n, errno, strerror(errno));
-          return 0;
-      }
     }
   }
 }
