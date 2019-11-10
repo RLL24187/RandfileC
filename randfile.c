966c7f53a9c5880ec795654b5ffc6f4354de5608
rleongPd5 <rleong00@stuy.edu>
2019-11-09 21:05:31 -0500

    moved the writing and second reading to main

--- randfile.c
+++ randfile.c
@@ -12,34 +12,18 @@ int rand()
   if (randomData < 0)
   {
       // ERROR
-      printf("Open errno %d error: %s\n", errno, strerror(errno));
+      printf("Open rand errno %d error: %s\n", errno, strerror(errno));
       return 0;
   }
   // int *rnd;
-  char rnd[100];
+  int rnd[1];
   int test = read(randomData, rnd, sizeof(rnd));
   if (test < 0)
   {
       // returning -1 and sets errno if unsuccessful
-      printf("Read errno %d error: %s\n", errno, strerror(errno));
+      printf("Read rand errno %d error: %s\n", errno, strerror(errno));
       return 0;
   }
   close(randomData);
-  int result;
-  result = open("randResult.txt", O_CREAT |O_RDWR, 0644); //opens and creates the file
-  if (result < 0)
-  {
-      // ERROR
-      printf("Open # 2 errno %d error: %s\n", errno, strerror(errno));
-      return 0;
-  }
-  int *buff = (int *)rnd;
-  randomData = write(result, buff, sizeof(int));
-  if (randomData < 0)
-  {
-      // ERROR
-      printf("Write errno %d error: %s\n", errno, strerror(errno));
-      return 0;
-  }
-  return *(int *)rnd;
+  return rnd[0];
 }
